b4346d3d1a69716b4548947437db893fc26d9107
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2020-12-08 23:03:06 +0900

    Third Commit
    This is third commit.

---
+++ app/controllers/edits_controller.rb
@@ -0,0 +1,29 @@
+class EditsController < ApplicationController
+
+    before_action :authenticate_user!
+    layout "edit"
+  
+    def index
+        limit_normal 1
+    end
+
+    def authentication_update
+        limit_strong
+        # データ更新のためのフォーム送信はPOSTではなくPATCHであることに注意
+        obj = User.find(params[:user_id])
+        obj.level = params[:level]
+        obj.save
+        redirect_to "/edits/authentication"
+    end
+  
+    def authentication
+        limit_strong
+        @msg = "Please type search word..."
+        if request.post? then
+        f = '%' + params["find"] + '%'
+        @users = User.where "email like ?", f
+        # @users = User.all
+        end
+    end
+
+end
---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,12 @@
+class HomesController < ApplicationController
+
+  layout "homes"
+
+  def index
+  end
+
+  def caution
+    @level = params["level"]
+  end
+  
+end
