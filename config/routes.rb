c940e2044cd534c1c7c1ce0cc5c0ef485952e785
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2020-12-09 01:13:12 +0900

    7th commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  
+  root to: 'homes#index'
+
+  get 'homes/index'
+  get 'homes/caution'
+  get 'homes/authentication'
+
+  get 'edits', to: 'edits#index'
+  
+  get 'edits/authentication'
+  post 'edits/authentication'
+
+  get 'edits/caution'
+
+  get 'edits/aupdate', to: 'edits#/authentication_update'
+  patch 'edits/aupdate', to: 'edits#/authentication_update'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
