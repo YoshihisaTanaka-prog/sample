0dc3d75ff7ae5bf9ceab759a6ecb8d81939beb69
TanakaYoshihisa <1sc16346k@gmail.com>
2020-12-09 12:00:43 +0900

    change user list

--- app/controllers/edits_controller.rb
+++ app/controllers/edits_controller.rb
@@ -9,21 +9,27 @@ class EditsController < ApplicationController
 
     def authentication_update
         limit_strong
+
         # データ更新のためのフォーム送信はPOSTではなくPATCHであることに注意
         obj = User.find(params[:user_id])
         obj.level = params[:level]
         obj.save
+        
         redirect_to "/edits/authentication"
     end
   
     def authentication
+        
         limit_strong
         @msg = "Please type search word..."
+
         if request.post? then
-        f = '%' + params["find"] + '%'
-        @users = User.where "email like ?", f
-        # @users = User.all
+            f = '%' + params["find"] + '%'
+            @users = User.where "email like ?", f
+        else
+            @users = User.all
         end
+
     end
 
 end
