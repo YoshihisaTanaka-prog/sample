d87d3b53440619bfbea71000ac984019db6dd76c
TanakaYoshihisa <1sc16346k@gmail.com>
2020-12-12 22:32:48 +0900

    17th commit

--- app/controllers/edits_controller.rb
+++ app/controllers/edits_controller.rb
@@ -2,13 +2,6 @@ class EditsController < ApplicationController
 
     before_action :authenticate_user!
     layout "edits"
-
-    def initialize
-        @@id = ""
-        @@title = ""
-        @@before_id = ""
-        @@ids = [""]
-    end
   
     def index
         limit_normal 1
@@ -18,36 +11,64 @@ class EditsController < ApplicationController
         @level = params["level"]
     end
 
+
+    # 編集系
+
     def edit
         limit_normal 2
-        @@id = params["id"]
-        route = Route.find(@@id)
+        @id = params["id"]
+        @route = Route.find(@id)
     end
 
     def edit_update
         limit_normal 2
 
-        obj = Route.find(@@id)
+        @id = params["id"]
+        @title = params["title"]
+        @before_id = params["before_id"]
+        obj = Route.find(@id)
         obj.table_name = params["table_name"]
         obj.next_title_name = params["next_title_name"]
         obj.save
 
-        redirect_to  controller: :edits, action: :show, params: {"title": @@title , "id": @@before_id}
+        redirect_to controller: :edits, action: :show, params: {"title": @title , "id": @before_id}
     end
 
-    def add
+
+    # 追加系
+
+    def add_route
         limit_normal 2
+
+        @route = Route.new
+    end
+
+    def create_route
+        limit_normal 2
+
+        if request.post? then
+
+            # 次のIDを取得
+            ids = params["ids"].split("-")
+            next_id = params["before_id"] + "-" + ((ids.last.to_i) + 1).to_s
+
+            obj = Route.new({ before_id: params["before_id"], table_name: params["table_name"], next_title_name: params["next_title_name"], after_id: next_id })
+            obj.save
+        end
+        @title = params["title"]
+        @before_id = params["before_id"]
+        redirect_to controller: :edits, action: :show, params: {"title": @title , "id": @before_id}
     end
 
     def show
         limit_normal 2
-        @@title = params["title"]
-        @@before_id = params["id"]
-        @tables = Route.where(before_id: @@before_id)
+        @title = params["title"]
+        @before_id = params["id"]
+        @tables = Route.where(before_id: @before_id)
 
-        @@ids = []
+        @ids = "0"
         @tables.each do |t|
-            @@ids.push(t.after_id)
+            @ids = t.after_id
         end
     end
 
@@ -66,14 +87,12 @@ class EditsController < ApplicationController
     end
   
     def authentication
-        
         limit_strong
 
         @msg = "Please type search word..."
     
         @q = User.ransack(params[:q])
         @users = @q.result(distinct: true)
-
     end
 
 end
